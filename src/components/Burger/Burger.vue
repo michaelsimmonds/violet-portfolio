6b91efba0d2a6c8e19f3e183326996bc4bd1eec4
Michael Simmonds <michaelsimmonds92@hotmail.com>
2020-09-25 13:38:04 +0100

    Refactored burger and made its active state dependent on nav open state via props

--- src/components/Burger/Burger.vue
+++ src/components/Burger/Burger.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="hamburger-menu">
-    <input type="checkbox" />
-    <span />
-    <span />
-    <span />
+  <div class="hamburger-menu" :class="{ 'checked': checked}">
+    <div class="hamburger-menu__wrapper">
+      <span class="hamburger-menu__span hamburger-menu__span--1" />
+      <span class="hamburger-menu__span hamburger-menu__span--2" />
+      <span class="hamburger-menu__span hamburger-menu__span--3" />
+    </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  props: {
+    checked: {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  }
+};
 </script>
 
 <style lang="scss" >
--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -2,23 +2,31 @@
   <div class="nav" :class="{ 'nav--open': !navOpen}">
     <div class="nav__wrapper">
       <div class="nav__home-wrapper">
-        <router-link to="/" class="nav__list-link">Violet Argent</router-link>
+        <router-link to="/" class="nav__list-link" @click.native="checkScrollLock">Violet Argent</router-link>
       </div>
-      <div class="nav__burger-wrapper" @click="toggleNav">
-        <Burger />
+      <div class="nav__burger-wrapper">
+        <Burger @click.native="toggleNav" :checked="navOpen" />
       </div>
     </div>
     <transition name="nav-slide">
       <div v-if="navOpen" class="nav__panel">
         <ul class="nav__list">
           <li class="nav__list-item">
-            <router-link to="/gallery" class="nav__list-link">Gallery</router-link>
+            <router-link
+              to="/gallery"
+              class="nav__list-link"
+              @click.native="checkScrollLock"
+            >Gallery</router-link>
           </li>
           <li class="nav__list-item">
-            <router-link to="/about" class="nav__list-link">About</router-link>
+            <router-link to="/about" class="nav__list-link" @click.native="checkScrollLock">About</router-link>
           </li>
           <li class="nav__list-item">
-            <router-link to="/contact" class="nav__list-link">Contact</router-link>
+            <router-link
+              to="/contact"
+              class="nav__list-link"
+              @click.native="checkScrollLock"
+            >Contact</router-link>
           </li>
         </ul>
       </div>
@@ -41,6 +49,13 @@ export default {
   methods: {
     toggleNav: function() {
       this.navOpen = !this.navOpen;
+      this.navOpen
+        ? (document.body.style.overflow = "hidden")
+        : (document.body.style.overflow = "auto");
+    },
+    checkScrollLock: function() {
+      if ((document.body.style.overflow = "hidden"))
+        document.body.style.overflow = "auto";
     }
   }
 };
