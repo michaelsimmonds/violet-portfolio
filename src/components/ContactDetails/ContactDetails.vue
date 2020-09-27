7674cb49d939069fb6d5686ab911ff0d0ec1382e
Michael Simmonds <michaelsimmonds92@hotmail.com>
2020-09-27 15:10:37 +0100

    Changed contact page styling

--- src/components/ContactDetails/ContactDetails.vue
+++ src/components/ContactDetails/ContactDetails.vue
@@ -1,33 +1,38 @@
 <template>
   <div class="contact-details">
-    <ul class="contact-details__list">
-      <li class="contact-details__list-item">
-        <a class="contact-details__link" href="mailto:violetiargent@gmail.com">
-          <img class="contact-details__icon" src="static/icons/email.svg" />
-          <span>violetiargent@gmail.com</span>
-        </a>
-      </li>
-      <li class="contact-details__list-item">
-        <a
-          class="contact-details__link"
-          href="https://www.instagram.com/viola.argent/"
-          target="_blank"
-        >
-          <img class="contact-details__icon" src="static/icons/instagram.svg" />
-          <span>@viola.argent</span>
-        </a>
-      </li>
-      <li class="contact-details__list-item">
-        <a
-          class="contact-details__link"
-          href="https://www.linkedin.com/in/violet-argent-8b512a136/"
-          target="_blank"
-        >
-          <img class="contact-details__icon" src="static/icons/linkedin.svg" />
-          <span>linkedin.com/in/violet-argent-8b512a136</span>
-        </a>
-      </li>
-    </ul>
+    <div class="contact-details__section contact-details__section--1">
+      <h2 class="contact-details__subtitle">Let's chat</h2>
+    </div>
+    <div class="contact-details__section">
+      <ul class="contact-details__list">
+        <li class="contact-details__list-item">
+          <a class="contact-details__link" href="mailto:violetiargent@gmail.com">
+            <img class="contact-details__icon" src="static/icons/email.svg" />
+            <span>violetiargent@gmail.com</span>
+          </a>
+        </li>
+        <li class="contact-details__list-item">
+          <a
+            class="contact-details__link"
+            href="https://www.instagram.com/viola.argent/"
+            target="_blank"
+          >
+            <img class="contact-details__icon" src="static/icons/instagram.svg" />
+            <span>@viola.argent</span>
+          </a>
+        </li>
+        <li class="contact-details__list-item">
+          <a
+            class="contact-details__link"
+            href="https://www.linkedin.com/in/violet-argent-8b512a136/"
+            target="_blank"
+          >
+            <img class="contact-details__icon" src="static/icons/linkedin.svg" />
+            <span>linkedin.com/in/violet-argent-8b512a136</span>
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
